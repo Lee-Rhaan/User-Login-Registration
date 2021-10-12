f5d549a8259f545ab590f0e3cac9c7c37a34711d
Lee-Rhaan <rhaanking@gmail.com>
2021-10-12 13:44:01 +0200

    Added Token authentication

--- security/src/main/java/com/example/security/registration/RegistrationService.java
+++ security/src/main/java/com/example/security/registration/RegistrationService.java
@@ -1,12 +1,29 @@
 package com.example.security.registration;
 
+import com.example.security.appuser.AppUser;
+import com.example.security.appuser.AppUserRole;
+import com.example.security.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
 
     public String register(RegistrationRequest request)
     {
-        return "it works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if(!isValidEmail)
+        {
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(request.getFirstName(), request.getLastName(),
+                            request.getEmail(), request.getPassword(), AppUserRole.USER));
     }
 }
